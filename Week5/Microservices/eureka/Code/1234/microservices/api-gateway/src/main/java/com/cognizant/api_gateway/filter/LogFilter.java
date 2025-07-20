bb5e53279beb8a0061b8a5597e71226aac66992f
Sarnesh K <118422138+sarnesh890@users.noreply.github.com>
2025-07-20 20:51:00 +0530

    Add files via upload

---
+++ Week5/Microservices/eureka/Code/1234/microservices/api-gateway/src/main/java/com/cognizant/api_gateway/filter/LogFilter.java
@@ -0,0 +1,22 @@
+package com.cognizant.api_gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String requestPath = exchange.getRequest().getPath().toString();
+        logger.info("Incoming request: {}", requestPath);
+        return chain.filter(exchange);
+    }
+}
